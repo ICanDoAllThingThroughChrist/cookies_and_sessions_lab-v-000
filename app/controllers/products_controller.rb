0abf2fefe6ebcf1a135cebd660b73c6d6bf295a3
charlie  lee <ctlee9096@gmail.com>
2017-11-24 18:31:55 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   helper_method :cart #inherited from ApplicationController
+  ##Load the cart from the session, or create a new empty cart.
   def index
   end
   def add_to_cart
